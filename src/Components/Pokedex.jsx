7977b89a1b7cc0143c421922443d2b282bbb8909
zoyron <sgrarora3@gmail.com>
2020-06-03 12:29:35 +0530

    grid setup done

--- src/Components/Pokedex.jsx
+++ src/Components/Pokedex.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { AppBar, Toolbar, Grid, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
+});
 const Pokedex = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h2>this is the pokedex</h2>
-    </div>
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar>Sagar's Pokedex </Toolbar>
+      </AppBar>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          1
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          2
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          3
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
 };
 
