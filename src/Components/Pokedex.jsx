c6721117a02684704e942178c2721776d919e02e
zoyron <sgrarora3@gmail.com>
2020-06-03 14:10:48 +0530

    text styling

--- src/Components/Pokedex.jsx
+++ src/Components/Pokedex.jsx
@@ -5,8 +5,11 @@ import {
   Grid,
   makeStyles,
   CircularProgress,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
 } from "@material-ui/core";
-import Pokecard from "./Pokecard";
 import mockData from "../mockData";
 
 const useStyles = makeStyles({
@@ -24,10 +27,48 @@ const useStyles = makeStyles({
     top: "50%",
     left: "45%",
   },
+  cardStyle: {
+    backgroundColor: "#f1f1f1",
+    borderRadius: "7.5%",
+  },
+  cardMedia: {
+    margin: "auto",
+  },
+  cardContent: {
+    textAlign: "center",
+  },
+  typoStyle: {
+    fontSize: "1.0625rem",
+    fontWeight: "bolder",
+    color: "#666",
+    letterSpacing: "0.25em",
+  },
 });
+
 const Pokedex = () => {
   const classes = useStyles();
   const [pokemonData, setPokemonData] = useState(mockData);
+  const Pokecard = (pokemonId) => {
+    const { id, name } = pokemonData[`${pokemonId}`];
+    const stripe = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    const classes = useStyles();
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3} key={pokemonId}>
+        <Card className={classes.cardStyle}>
+          <CardMedia
+            className={classes.cardMedia}
+            image={stripe}
+            style={{ width: "130px", height: "130px" }}
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography variant="button" className={classes.typoStyle}>
+              {name}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  };
   return (
     <React.Fragment>
       <AppBar position="static" className={classes.appbarStyle}>
@@ -35,10 +76,7 @@ const Pokedex = () => {
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={2} className={classes.pokedexContainer}>
-          <Pokecard />
-          <Pokecard />
-          <Pokecard />
-          <Pokecard />
+          {Object.keys(pokemonData).map((pokemonId) => Pokecard(pokemonId))}
         </Grid>
       ) : (
         <CircularProgress className={classes.progressStyle} />
